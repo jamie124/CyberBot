b5d43bf1f1daa552865ad5e17c8855c16ebab523
James Whitwell <jtwhitwell124@gmail.com>
2011-04-07 22:11:39 +1200

    Started adding file saving code for logs.

--- include/cyber_logger.h
+++ include/cyber_logger.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <iostream>
+#include <string>
+#include <fstream>
 #include <vector>
 
 class CyberLogger
@@ -9,6 +11,10 @@ private:
 	typedef std::vector<std::string>* MessageLog;
 
 	MessageLog messageLog;
+
+	const char* filePath;
+
+	std::ofstream fileOut;
 
 public:
 	CyberLogger(void);
--- src/cyber_logger.cpp
+++ src/cyber_logger.cpp
@@ -1,11 +1,22 @@
 #include "cyber_logger.h"
 
 CyberLogger::CyberLogger(void){
+	filePath = "main_log.log";
+
 	messageLog = new std::vector<std::string>;
+
+	fileOut.open(filePath, std::ios_base::app);
+
+	if(!fileOut.is_open()){
+		std::cout << "Error: could not open file for appending, creating a new one." << std::endl;
+		fileOut.open(filePath);
+	}
 }
 
 CyberLogger::~CyberLogger(void){
 	delete messageLog;
+
+	fileOut.close();
 }
 
 void CyberLogger::addMessage(std::string newMessage){
