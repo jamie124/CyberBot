97820c082afa0cc3c0a4bf77ac21bddeedfb20f0
James Whitwell <jtwhitwell124@gmail.com>
2011-04-09 19:21:57 +1200

    Added cyber entity code

---
+++ src/cyber_entity.cpp
@@ -0,0 +1,43 @@
+#include "cyber_entity.h"
+
+std::vector<CyberEntity*>CyberEntity::entityList;
+
+CyberEntity::CyberEntity(){
+	entitySurf = NULL;
+	x = y = 0.0f;
+	width = height = 0;
+	animationState = 0;
+}
+
+CyberEntity::~CyberEntity(){
+}
+
+bool CyberEntity::onLoad(char* file, int width, int height, int maxFrames){
+	if ((entitySurf = CyberSurface::onLoad(file)) == NULL){
+		return false;
+	}
+
+	this->width = width;
+	this->height = height;
+	animationControl.maxFrames = maxFrames;
+
+	return true;
+}
+
+void CyberEntity::onLoop(){
+	animationControl.onAnimate();
+}
+
+void CyberEntity::onRender(SDL_Surface* displaySurf){
+	if (entitySurf == NULL || displaySurf == NULL) return;
+
+	CyberSurface::onDraw(displaySurf, entitySurf, x, y, animationState * width, animationControl.getCurrentFrame() * height, width, height);
+}
+
+void CyberEntity::onCleanup(){
+	if (entitySurf){
+		SDL_FreeSurface(entitySurf);
+	}
+
+	entitySurf = NULL;
+}
